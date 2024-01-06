eb7e7d06892964c5b0c63d907f07450398342207
Assertivedreamer <assertivedreamer@gmail.com>
2024-01-06 15:18:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
+
+
+// app = require("../app")
+
+var express = require('express');
+
+
+var app = express();
+
+var port = "1000";
+app.set('port', port);
+
+
+
+// const express=require("express");
+const mongoose= require("mongoose");
+// const app=express();
+const router = require("./routes/router");
+
+var cors=require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+// const url="mongodb+srv://admin:4LPZudtJGHHLK2Xx@cluster0.5qu8lrg.mongodb.net/?retryWrites=true&w=majority"
+const url2="mongodb+srv://youtube:admin123@cluster0.g1xfhn1.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(url2).then(()=>{
+    console.log("Database Connected Succssfully")
+}).catch((err)=>{
+    console.log(err)
+});
+
+app.listen(5000);
+
+module.exports = app;
